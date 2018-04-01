31a56bff63f28072e60bc7b3d6f38d70a3418c9d
Jonathan Soifer <jonathan.soifer@movinga.com>
2018-04-01 15:57:38 +0200

    fix app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "fake_twiter_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "fake_twitter_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
